014eca221604a04fa781ccc76e632097892e57e7
ヨウスケ <tokyosoul@MacBook-2.local>
2012-10-10 02:03:31 +0900

    first

---
+++ eyes/Classes/titleView.h
@@ -0,0 +1,16 @@
+//
+//  titleView.h
+//  eyes
+//
+//  Created by ヨウスケ on 10/01/24.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface titleView : UIView {
+ IBOutlet UIButton *start;
+}
+
+@end
---
+++ eyes/Classes/titleView.m
@@ -0,0 +1,33 @@
+//
+//  titleView.m
+//  eyes
+//
+//  Created by ヨウスケ on 10/01/24.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+#import "eyesAppDelegate.h"
+#import "titleView.h"
+
+
+@implementation titleView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
